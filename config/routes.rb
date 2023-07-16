8d5d3fbbb4f08496a885f365acd64fe34d7dfb43
EC2 Default User <ec2-user@ip-172-31-5-135.ap-northeast-1.compute.internal>
2023-07-16 09:11:16 +0000

    nattou2

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,9 @@
 Rails.application.routes.draw do
-  devise_for :admins
-  devise_for :users
+  resources :items, only: [:index,:show]
+  
+  namespace :admin do
+  resources :items
+  
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+ end
 end
