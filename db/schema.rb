50c71cf394ed5d21c56b4ea115321d0b6d150c79
KoudaiKatayama <132341723+koudaikatayama@users.noreply.github.com>
2023-07-18 07:23:26 +0000

    customer_model

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2023_07_17_051933) do
+ActiveRecord::Schema.define(version: 2023_07_18_063359) do
 
   create_table "admins", force: :cascade do |t|
     t.datetime "created_at", precision: 6, null: false
@@ -22,6 +22,14 @@ ActiveRecord::Schema.define(version: 2023_07_17_051933) do
     t.datetime "remember_created_at"
     t.index ["email"], name: "index_admins_on_email", unique: true
     t.index ["reset_password_token"], name: "index_admins_on_reset_password_token", unique: true
+  end
+
+  create_table "cart_items", force: :cascade do |t|
+    t.integer "item_id", null: false
+    t.integer "customer_id", null: false
+    t.integer "quantity", null: false
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
   end
 
   create_table "customers", force: :cascade do |t|
@@ -42,6 +50,29 @@ ActiveRecord::Schema.define(version: 2023_07_17_051933) do
     t.datetime "updated_at", precision: 6, null: false
     t.index ["email"], name: "index_customers_on_email", unique: true
     t.index ["reset_password_token"], name: "index_customers_on_reset_password_token", unique: true
+  end
+
+  create_table "items", force: :cascade do |t|
+    t.integer "genre_id", null: false
+    t.integer "without_tax_price", null: false
+    t.string "name", null: false
+    t.text "explanation", null: false
+    t.boolean "is_sale", default: true, null: false
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+  end
+
+  create_table "orders", force: :cascade do |t|
+    t.integer "customer_id", null: false
+    t.integer "total_payment", null: false
+    t.integer "shipping_fee", null: false
+    t.integer "status", default: 0, null: false
+    t.string "name", null: false
+    t.string "address", null: false
+    t.string "post_code", null: false
+    t.integer "payment_method", default: 0, null: false
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
   end
 
 end
