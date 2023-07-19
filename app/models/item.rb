fe88b7bf38007d35423922597c97041c61c35b32
takumi19991120 <135395048+takumi19991120@users.noreply.github.com>
2023-07-19 12:16:28 +0000

    [middle]admin-view

--- app/models/item.rb
+++ app/models/item.rb
@@ -1,6 +1,11 @@
 class Item < ApplicationRecord
-  
+
   belongs_to :genre
   has_many :cart_items, dependent: :destroy
   has_many :order_items, dependent: :destroy
+
+  def add_tax_sales_price
+   (self.without_tax_price * 1.10).round
+  end
+  
 end
