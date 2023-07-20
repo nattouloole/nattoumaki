757bbcb797b96c2dd8acdff5d850d70522ab5c0f
takumi19991120 <135395048+takumi19991120@users.noreply.github.com>
2023-07-20 09:35:54 +0000

    [middle]admin_view

--- app/controllers/admin/orders_controller.rb
+++ app/controllers/admin/orders_controller.rb
@@ -1,9 +1,27 @@
 class Admin::OrdersController < ApplicationController
+  def index
+    @orders = Order.all.page(params[:page]).per(10)
+  end
+  
   def show
+    @customer = current_customer
+    @order_items = @customer.order_items.all
+    @total = 0
+    @totals = @order_items.inject(0) { |sum, order_items| sum + order_items.subtotal }
+    @shipping_fee = 800
     
   end
   
   def update
-    
+    @order = Order.find(params[:id])
+    @order.update(order_params)
+    redirect_back(fallback_location: root_path)
   end
+  
+  private
+
+  def order_params
+    params.require(:order).permit(:customer_id, :total_payment, :shipping_fee, :status, :name, :address, :post_code, :payment_method)
+  end
+  
 end
--- app/controllers/admin/orders_details_controller.rb
+++ app/controllers/admin/orders_details_controller.rb
@@ -1,5 +1,15 @@
 class Admin::OrdersDetailsController < ApplicationController
   def update
-    
+    @order_details = Order_items.all
+    @order_details.update(order_details_params)
+    redirect_back(fallback_location: root_path)
   end
+  
+  private
+
+  def order_details_params
+    params.require(:order_items).permit(:order_id, :item_id, :quantity, :price, :making_status)
+  end
+  
 end
+
