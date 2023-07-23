4e097b79a4aaa1e56d67b8085bd82c6a49b60f55
Syuji-gooner8 <135284715+Syuji-gooner8@users.noreply.github.com>
2023-07-23 12:35:14 +0000

    publicB

--- app/controllers/public/orders_controller.rb
+++ app/controllers/public/orders_controller.rb
@@ -1,33 +1,33 @@
 class Public::OrdersController < ApplicationController
   
-  def new
-    @order = Order.new
-    @addresses = Address.all
-  end
-  
-  def comfirm
-    @order = Order.new(params)
-    if params[:order][:select_address] == "0"
-      @order.post_code = current_end_user.post_code
-      @order.address = current_end_user.address
-      @order.name = current_end_user.first_name + current_end_user.last_name
-      
-    elsif params[:order][:select_address] == "1"
-       @address = Address.find(params[:order][:address_id])
-       @order.post_code = @address.post_code
-       @order.address = @address.address
-       @order.name = @address.name
-       
-    elsif params[:order][:select_address] == "2"
-      @order.end_user_id = current_end_user.id
-    end
-      @cart_items = current_end_user.cart_items
-      @order_new = Order.new
-      render :confirm
-  end
-  
-  def complete
+def new
+@order = Order.new
+@addresses = Address.all
+end
+
+    def comfirm
+    @order = Order.new(order_params)
+    if params[:order][:address] == "0"
+    @order.post_code = post_code
+    @order.address = address
+    @order.name = first_name + last_name
     
+    elsif params[:order][:address] == "1"
+    @address = Address.find(params[:order][:address_id])
+    @order.post_code = @address.post_code
+    @order.address = @address.address
+    @order.name = @address.name
+    
+    elsif params[:order][:address] == "2"
+    @order = address
+    end
+    @cart_items = cart_items_path
+    @order_new = Order.new
+    render :comfirm
+    end
+
+def complete
+
   end
   
   def create
