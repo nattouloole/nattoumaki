837425972d3aee4b62fe7d51f6414eafda682fc7
Syuji-gooner8 <135284715+Syuji-gooner8@users.noreply.github.com>
2023-07-23 05:57:18 +0000

    publicB

--- app/controllers/public/orders_controller.rb
+++ app/controllers/public/orders_controller.rb
@@ -1,12 +1,17 @@
 class Public::OrdersController < ApplicationController
   
   def new
+    @order = Order.new
+    @addresses = Address.all
   end
   
   def comfirm
+    @order = Order.new
+    
   end
   
   def complete
+    
   end
   
   def create
