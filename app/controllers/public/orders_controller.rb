da3d27a00c989bcb47e413012b4363370a41327a
Syuji-gooner8 <135284715+Syuji-gooner8@users.noreply.github.com>
2023-07-24 07:20:15 +0000

    publicB

--- app/controllers/public/orders_controller.rb
+++ app/controllers/public/orders_controller.rb
@@ -1,46 +1,50 @@
 class Public::OrdersController < ApplicationController
-  
+
 def new
 @order = Order.new
 @addresses = Address.all
 end
 
-    def comfirm
+  def comfirm
     @order = Order.new(order_params)
     if params[:order][:address] == "0"
-    @order.post_code = post_code
-    @order.address = address
-    @order.name = first_name + last_name
-    
+      @order.post_code = post_code
+      @order.address = address
+      @order.name = first_name + last_name
+
     elsif params[:order][:address] == "1"
-    @address = Address.find(params[:order][:address_id])
-    @order.post_code = @address.post_code
-    @order.address = @address.address
-    @order.name = @address.name
-    
+      @address = Address.find(params[:order][:address_id])
+      @order.post_code = @address.post_code
+      @order.address = @address.address
+      @order.name = @address.name
+
     elsif params[:order][:address] == "2"
-    @order = address
+      @order.post_code = params[:order][:post_code]
+      @order.address = params[:order][:address]
+      @order.name = params[:order][:name]
+    else
+      #render :comfirm
     end
-    @cart_items = cart_items_path
+    
+    @cart_items = CartItem.all
     @order_new = Order.new
-    render :comfirm
-    end
+  end
 
   def complete
 
   end
-  
+
   def create
   end
-  
+
   def index
   end
-  
+
   def show
   end
-  
+
   private
-  
+
   def order_params
     params.require(:order).permit(:total_payment, :shipping_fee, :status, :name, :address, :post_code, :payment_method)
   end
