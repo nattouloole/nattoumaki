90aaee303d59a62f1d785ffd9362ac05e350eadc
EC2 Default User <ec2-user@ip-172-31-5-135.ap-northeast-1.compute.internal>
2023-07-24 07:57:21 +0000

    public.13

--- app/controllers/admin/orders_details_controller.rb
+++ app/controllers/admin/orders_details_controller.rb
@@ -1,8 +1,21 @@
 class Admin::OrdersDetailsController < ApplicationController
+  
   def update
     @customer = current_customer
-    @order_items = Order_items.all
-    @order_items.update(order_details_params)
+    @order = Order.find(params[:order_id])
+    @order_item = OrderItem.find(params[:id])
+    @order_items = @order.order_items.all
+    
+    is_updated = true
+    if @order_item.update(order_detail_params)
+      @order.update(status: 2) if @order_item.making_status == "in_production"
+      @order_items.each do |order_item|
+        if order_item.making_status != "production_complete"
+          is_updated = false
+        end
+      end
+      @order.update(status: 3) if is_updated
+    end
     redirect_back(fallback_location: root_path)
   end
   
