1b5d26899b4761c536ae5222fbf272f44b6fde47
takumi19991120 <135395048+takumi19991120@users.noreply.github.com>
2023-07-25 11:39:34 +0000

    [Finish]debug3

--- app/controllers/public/orders_controller.rb
+++ app/controllers/public/orders_controller.rb
@@ -6,6 +6,8 @@ def new
 end
 
   def comfirm
+    @cart_items = current_customer.cart_items
+    @total = @cart_items.inject(0) { |sum, item| sum + item.sum_of_price }
     @order = Order.new(order_params)
     if params[:order][:address] == "0"
       @order.post_code = post_code
@@ -26,7 +28,7 @@ end
       @cart_items = CartItem.all
       render :comfirm
     end
-    
+
     @cart_items = CartItem.all
     @order_new = Order.new
   end
@@ -36,8 +38,18 @@ end
   end
 
   def create
-    @order = Order.new(order_params)
+    @order = current_customer.orders.new(order_params)
     @order.save
+  
+   current_customer.cart_items.each do |cart_item|
+    @order_item = OrderItem.new
+    @order_item.order_id = @order.id
+    @order_item.item_id = cart_item.item.id
+    @order_item.price = cart_item.item.add_tax_sales_price
+    @order_item.quantity = cart_item.quantity
+    @order_item.save
+   end
+    current_customer.cart_items.destroy_all
     redirect_to orders_complete_path
   end
 
