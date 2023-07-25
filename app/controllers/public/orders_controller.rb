50c30a97a7993f3191cd11408461d8476cd33ad4
EC2 Default User <ec2-user@ip-172-31-5-135.ap-northeast-1.compute.internal>
2023-07-25 04:17:44 +0000

    public.12

--- app/controllers/public/orders_controller.rb
+++ app/controllers/public/orders_controller.rb
@@ -11,13 +11,13 @@ class Public::OrdersController < ApplicationController
       @order.post_code = current_end_user.post_code
       @order.address = current_end_user.address
       @order.name = current_end_user.first_name + current_end_user.last_name
-      
+
     elsif params[:order][:select_address] == "1"
        @address = Address.find(params[:order][:address_id])
        @order.post_code = @address.post_code
        @order.address = @address.address
        @order.name = @address.name
-       
+
     elsif params[:order][:select_address] == "2"
       @order.end_user_id = current_end_user.id
     end
@@ -27,7 +27,7 @@ class Public::OrdersController < ApplicationController
   end
 
   def complete
-    
+
   end
 
   def create
@@ -45,14 +45,10 @@ class Public::OrdersController < ApplicationController
     @totals = @order_items.inject(0) { |sum, order_items| sum + order_items.subtotal }
     @shipping_fee = 800
   end
-<<<<<<< HEAD
 
-=======
-  
   private
-  
+
   def order_params
     params.require(:order).permit(:total_payment, :shipping_fee, :status, :name, :address, :post_code, :payment_method)
   end
->>>>>>> origin/develop
 end
